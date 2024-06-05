d328f65a0b63aa224a90b6006c5e787a6ce30dee
Nityananda <pnityananda2001@gmail.com>
2024-06-05 14:22:21 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3500;
+app.get("/api", (req, res) => {
+  res.send({ message: "Hello from Express!" });
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
